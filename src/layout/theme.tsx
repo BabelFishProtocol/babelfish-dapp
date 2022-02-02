e245aa50d35b2abc53d3ab8f6b25d91b230d329c
Michal Topa <michal.t1506@gmail.com>
2022-02-02 17:33:37 +0100

    mui configuration

---
+++ src/layout/theme.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { createTheme, responsiveFontSizes, ThemeProvider as MuiThemeProvider, Theme, Components } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline';
+import createPalette from '@mui/material/styles/createPalette'
+
+/* @ts-ignore */
+import Comfortaa from './fonts/Comfortaa-Regular.ttf';
+/* @ts-ignore */
+import Heavitas from './fonts/Heavitas.ttf';
+
+const palette = createPalette({
+  primary: {
+    main: "#ffbf42"
+  },
+  background: {
+    default: "#181a20",
+  }
+})
+
+const components: Components<Theme> = {
+  MuiCssBaseline: {
+    styleOverrides: `
+      @font-face {
+        font-family: 'Comfortaa-Regular';
+        font-style: normal;
+        font-display: swap;
+        font-weight: 400;
+        src: url(${Comfortaa}) format('truetype');
+        unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+      }
+      @font-face {
+        font-family: 'Heavitas';
+        font-style: normal;
+        font-display: swap;
+        font-weight: 400;
+        src: url(${Heavitas}) format('truetype');
+        unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+      }
+    `,
+  },
+}
+
+const theme = responsiveFontSizes(
+  createTheme({
+    palette,
+    components,
+    typography: {
+      fontSize: 14,
+      fontFamily: [
+        'Heavitas',
+      ].join(',')
+    },
+  })
+)
+
+export const ThemeProvider: React.FC = ({ children }) => (
+  <MuiThemeProvider theme={theme}>
+    <CssBaseline />
+    {children}
+  </MuiThemeProvider>
+)
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
+import { ThemeProvider } from './layout/theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
