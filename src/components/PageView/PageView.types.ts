aba4e6c7f619c5dcb79466e2af4b1c5bbfcc2afe
Michał Topa <michal.t1506@gmail.com>
2022-02-03 16:18:55 +0100

    move styled tags to .styles files

---
+++ src/components/PageView/PageView.types.ts
@@ -0,0 +1,4 @@
+export type PageViewProps = {
+  title: React.ReactNode;
+  children: React.ReactNode;
+};
--- src/pages/ProposalsList/ProposalsList.component.tsx
+++ src/pages/ProposalsList/ProposalsList.component.tsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 
 import MuiLink from '@mui/material/Link';
 import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
 import Badge from '@mui/material/Badge';
 import Typography from '@mui/material/Typography';
 
@@ -12,44 +11,11 @@ import {
   DataTableColumn,
 } from '../../components/DataTable/DataTable.types';
 import { DataTable } from '../../components/DataTable/DataTable.component';
+import { PageView } from '../../components/PageView/PageView.component';
 import { Urls } from '../../constants';
 
+import { ButtonContainer } from './ProposalsList.styles';
 import { ProposalsListComponentProps } from './ProposalsList.types';
-
-const PageContent = styled('div')(({ theme }) => ({
-  borderRadius: 8,
-  backgroundImage: theme.palette.boxGradient,
-  width: '80%',
-  maxWidth: 1100,
-  [theme.breakpoints.down('sm')]: {
-    width: '100%',
-  },
-}));
-
-const PageAligner = styled('div')({
-  width: '100%',
-  height: '100vh',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-});
-
-const PageHeader = styled('div')(({ theme }) => ({
-  borderBottom: `1px solid ${theme.palette.borderGrey.main}`,
-  padding: theme.spacing(1),
-  paddingBottom: 6,
-}));
-
-const ContentContainer = styled('div')(({ theme }) => ({
-  padding: `${theme.spacing(4)} ${theme.spacing(3)}`,
-}));
-
-const ButtonContainer = styled('div')(({ theme }) => ({
-  width: '100%',
-  paddingTop: theme.spacing(3),
-  display: 'flex',
-  justifyContent: 'center',
-}));
 
 const ViewProposalComponent: CustomColumn = ({ value }) => {
   return (
@@ -65,7 +31,6 @@ const ViewProposalComponent: CustomColumn = ({ value }) => {
 
 const VoteWeightComponent: CustomColumn = ({ value, rowData }) => {
   const dotColor = rowData.isWinning === 'true' ? 'success' : 'error';
-
   return (
     <>
       <Badge variant="dot" sx={{ marginRight: 2 }} color={dotColor} />
@@ -88,25 +53,23 @@ export const ProposalsListComponent = ({
   proposals,
 }: ProposalsListComponentProps) => {
   return (
-    <PageAligner>
-      <PageContent>
-        <PageHeader>
-          <Typography variant="h2" padding={1}>
-            BABELFISH BITOCRACY
-          </Typography>
-        </PageHeader>
-        <ContentContainer>
-          <DataTable
-            data={proposals}
-            columns={proposalsListColumns}
-            tableTitle="GOVERNANCE PROPOSALS"
-            tableAction={<Button variant="text">+ CREATE PROPOSAL</Button>}
-          />
-          <ButtonContainer>
-            <Button variant="outlined">View All Proposals</Button>
-          </ButtonContainer>
-        </ContentContainer>
-      </PageContent>
-    </PageAligner>
+    <PageView
+      title={
+        <Typography variant="h2" padding={1}>
+          BABELFISH BITOCRACY
+        </Typography>
+      }
+    >
+      <DataTable
+        data={proposals}
+        columns={proposalsListColumns}
+        tableTitle="GOVERNANCE PROPOSALS"
+        tableAction={<Button variant="text">+ CREATE PROPOSAL</Button>}
+      />
+
+      <ButtonContainer>
+        <Button variant="outlined">View All Proposals</Button>
+      </ButtonContainer>
+    </PageView>
   );
 };
