a694025921c9a95cc34e95d5cc26c22db360ce57
Michal Topa <michal.t1506@gmail.com>
2022-02-07 14:55:34 +0100

    add loading state to dataTable

--- src/components/DataTable/DataTable.component.tsx
+++ src/components/DataTable/DataTable.component.tsx
@@ -5,10 +5,70 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import Skeleton from '@mui/material/Skeleton';
 import Typography from '@mui/material/Typography';
 import TableContainer from '@mui/material/TableContainer';
 
-import { DataTableProps, DataTableRowProps } from './DataTable.types';
+import {
+  DataTableProps,
+  DataTableRowProps,
+  LoadingStateRowProps,
+} from './DataTable.types';
+
+export const DataTable = ({
+  data,
+  columns,
+  isLoading,
+  tableTitle,
+  tableAction,
+  containerSx,
+}: DataTableProps) => (
+  <TableContainer
+    sx={{
+      maxHeight: 400,
+      borderRadius: 2,
+      backgroundImage: (theme) => theme.palette.boxGradient,
+      ...containerSx,
+    }}
+  >
+    <Table>
+      <TableHead>
+        <TableRow sx={{ border: 'none' }}>
+          <TableCell colSpan={columns.length - 1}>
+            <Typography variant="h4">{tableTitle}</Typography>
+          </TableCell>
+          <TableCell colSpan={1}>{tableAction}</TableCell>
+        </TableRow>
+      </TableHead>
+
+      <TableHead>
+        <TableRow>
+          {columns.map(({ label }, headIndex) => (
+            <TableCell key={headIndex}>{label}</TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+
+      <TableBody>
+        {isLoading && <LoadingStateRow columns={columns} />}
+        {!isLoading &&
+          data.map((rowData, rowIndex) => (
+            <DataTableRow
+              key={rowIndex}
+              columns={columns}
+              rowIndex={rowIndex}
+              rowData={rowData}
+            />
+          ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+);
+
+DataTable.defaultProps = {
+  isLoading: false,
+  containerSx: {},
+};
 
 const DataTableRow = ({ rowIndex, columns, rowData }: DataTableRowProps) => (
   <TableRow key={rowIndex}>
@@ -33,47 +93,12 @@ const DataTableRow = ({ rowIndex, columns, rowData }: DataTableRowProps) => (
   </TableRow>
 );
 
-export const DataTable = ({
-  data,
-  columns,
-  tableTitle,
-  tableAction,
-  containerSx = {},
-}: DataTableProps) => (
-  <TableContainer
-    sx={{
-      maxHeight: 400,
-      borderRadius: 2,
-      backgroundImage: (theme) => theme.palette.boxGradient,
-      ...containerSx,
-    }}
-  >
-    <Table>
-      <TableHead>
-        <TableRow sx={{ border: 'none' }}>
-          <TableCell colSpan={columns.length - 1}>
-            <Typography variant="h4">{tableTitle}</Typography>
-          </TableCell>
-          <TableCell colSpan={1}>{tableAction}</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableHead>
-        <TableRow>
-          {columns.map(({ label }, headIndex) => (
-            <TableCell key={headIndex}>{label}</TableCell>
-          ))}
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {data.map((rowData, rowIndex) => (
-          <DataTableRow
-            key={rowIndex}
-            columns={columns}
-            rowIndex={rowIndex}
-            rowData={rowData}
-          />
-        ))}
-      </TableBody>
-    </Table>
-  </TableContainer>
+const LoadingStateRow = ({ columns }: LoadingStateRowProps) => (
+  <TableRow>
+    {columns.map((_, cellIndex) => (
+      <TableCell key={cellIndex}>
+        <Skeleton />
+      </TableCell>
+    ))}
+  </TableRow>
 );
