87eca0c459c6858839998a17e74320c442e8947d
Adil <mohammadadil610@gmail.com>
2022-02-24 01:50:05 +0500

    integrating VotingButton into proposal details

--- src/components/VotingButton/VotingButton.component.tsx
+++ src/components/VotingButton/VotingButton.component.tsx
@@ -7,8 +7,17 @@ import { VoteType } from '../../constants';
 import { AppDialog } from '../AppDialog/AppDialog.component';
 import { VotingButtonProps } from './VotingButton.types';
 
-export const VotingButton = ({ type, sx, onClick }: VotingButtonProps) => {
+export const VotingButton = ({
+  type,
+  sx,
+  isLoading,
+  onClick,
+  isVoted,
+  disabled,
+}: VotingButtonProps) => {
   const [isVoting, setIsVoting] = useState(false);
+
+  const isVoteFor = type === VoteType.FOR;
 
   const onButtonClick = () => {
     setIsVoting(true);
@@ -37,54 +46,43 @@ export const VotingButton = ({ type, sx, onClick }: VotingButtonProps) => {
         </Button>
       </AppDialog>
 
-      {type === VoteType.FOR && (
-        <Button
-          color="success"
-          variant="outlined"
-          sx={({ palette }) => ({
-            boxShadow: `inset 0 0 0 2px ${palette.success.main}`,
-            backgroundColor: 'rgba(50, 240, 95, 0.1)',
-            ':hover': {
-              backgroundColor: `rgba(50, 240, 95, 0.2)`,
-            },
-            ...sx,
-          })}
-          onClick={onButtonClick}
-        >
-          {isVoting && (
-            <>
-              <CircularProgress color="success" size={20} sx={{ mr: 1 }} />
-              Voting
-            </>
-          )}
+      <Button
+        color={isVoteFor ? 'success' : 'error'}
+        variant="outlined"
+        disabled={disabled || isLoading || isVoting}
+        sx={({ palette }) => ({
+          boxShadow: `inset 0 0 0 2px ${
+            isVoteFor ? palette.success.main : palette.error.main
+          }`,
+          backgroundColor: isVoteFor
+            ? 'rgba(50, 240, 95, 0.1)'
+            : 'rgba(239, 5, 18, 0.1)',
+          ':hover': {
+            backgroundColor: isVoteFor
+              ? `rgba(50, 240, 95, 0.2)`
+              : `rgba(239, 5, 18, 0.2)`,
+          },
+          ...sx,
+        })}
+        onClick={onButtonClick}
+      >
+        {!isVoted && (isLoading || isVoting) && (
+          <>
+            <CircularProgress
+              color={isVoteFor ? 'success' : 'error'}
+              size={20}
+              sx={{ mr: 1 }}
+            />
+            Voting
+          </>
+        )}
 
-          {!isVoting && <>Vote For</>}
-        </Button>
-      )}
-      {type === VoteType.AGAINST && (
-        <Button
-          color="error"
-          variant="outlined"
-          sx={({ palette }) => ({
-            boxShadow: `inset 0 0 0 2px ${palette.error.main}`,
-            backgroundColor: `rgba(239, 5, 18, 0.1)`,
-            ':hover': {
-              backgroundColor: `rgba(239, 5, 18, 0.2)`,
-            },
-            ...sx,
-          })}
-          onClick={onButtonClick}
-        >
-          {isVoting && (
-            <>
-              <CircularProgress color="error" size={20} sx={{ mr: 1 }} />
-              Voting
-            </>
-          )}
-
-          {!isVoting && <>Vote Against</>}
-        </Button>
-      )}
+        {!isVoted &&
+          !isLoading &&
+          !isVoting &&
+          (isVoteFor ? 'Vote For' : 'Vote Against')}
+        {isVoted && <>Voted &#10003;</>}
+      </Button>
     </Box>
   );
 };
--- src/components/VotingButton/VotingButton.types.ts
+++ src/components/VotingButton/VotingButton.types.ts
@@ -4,5 +4,8 @@ import { VoteType } from '../../constants';
 export type VotingButtonProps = {
   type: VoteType;
   sx?: Partial<ButtonProps['sx']>;
+  isVoted?: boolean;
+  isLoading?: boolean;
+  disabled?: boolean;
   onClick?: () => void;
 };
