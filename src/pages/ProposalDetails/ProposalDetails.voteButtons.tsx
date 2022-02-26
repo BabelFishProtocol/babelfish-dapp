d0891dc85566773b5bbf2295eafa6c0f55865a48
Adil <mohammadadil610@gmail.com>
2022-02-26 15:47:12 +0500

    Simplifying Voting Buttons

--- src/pages/ProposalDetails/ProposalDetails.voteButtons.tsx
+++ src/pages/ProposalDetails/ProposalDetails.voteButtons.tsx
@@ -1,6 +1,10 @@
-import { VotingButton } from '../../components/VotingButton/VotingButton.component';
-import { ProposalState, VoteType } from '../../constants';
+import { useState } from 'react';
+import { Button } from '../../components/Button/Button.component';
+import { VotingDialog } from '../../components/VotingDialog/VotingDialog.component';
+import { ProposalState } from '../../constants';
 import { VoteButtonProps } from './ProposalDetails.types';
+
+const VotedText = () => <>voted &#10003;</>;
 
 export const VoteForButton = ({
   voteStatus,
@@ -10,18 +14,40 @@ export const VoteForButton = ({
 
   const votedFor = type === 'for' && status === 'success';
   const showButton = proposalState === ProposalState.Active || votedFor;
+  const [showDialog, setShowDialog] = useState(false);
 
   if (!showButton) {
     return null;
   }
 
   return (
-    <VotingButton
-      type={VoteType.FOR}
-      isLoading={type === 'for' && status === 'loading'}
-      isVoted={votedFor}
-      disabled={status !== 'idle'}
-    />
+    <>
+      <VotingDialog
+        isOpenDialog={showDialog}
+        onClose={() => {
+          setShowDialog(false);
+        }}
+      />
+      <Button
+        color="success"
+        variant="outlined"
+        loadingText="Voting..."
+        disabled={status !== 'idle'}
+        onClick={() => {
+          setShowDialog(true);
+        }}
+        isLoading={type === 'for' && status === 'loading'}
+        sx={({ palette }) => ({
+          boxShadow: `inset 0 0 0 2px ${palette.success.main}`,
+          backgroundColor: 'rgba(50, 240, 95, 0.1)',
+          ':hover': {
+            backgroundColor: `rgba(50, 240, 95, 0.2)`,
+          },
+        })}
+      >
+        {votedFor ? <VotedText /> : 'Vote For This'}
+      </Button>
+    </>
   );
 };
 
@@ -33,17 +59,39 @@ export const VoteAgainstButton = ({
 
   const votedAgainst = type === 'against' && status === 'success';
   const showButton = proposalState === ProposalState.Active || votedAgainst;
+  const [showDialog, setShowDialog] = useState(false);
 
   if (!showButton) {
     return null;
   }
 
   return (
-    <VotingButton
-      type={VoteType.AGAINST}
-      isLoading={type === 'against' && status === 'loading'}
-      isVoted={votedAgainst}
-      disabled={status !== 'idle'}
-    />
+    <>
+      <VotingDialog
+        isOpenDialog={showDialog}
+        onClose={() => {
+          setShowDialog(false);
+        }}
+      />
+      <Button
+        color="error"
+        variant="outlined"
+        loadingText="Voting..."
+        isLoading={type === 'against' && status === 'loading'}
+        disabled={status !== 'idle'}
+        onClick={() => {
+          setShowDialog(true);
+        }}
+        sx={({ palette }) => ({
+          boxShadow: `inset 0 0 0 2px ${palette.error.main}`,
+          backgroundColor: `rgba(239, 5, 18, 0.1)`,
+          ':hover': {
+            backgroundColor: `rgba(239, 5, 18, 0.2)`,
+          },
+        })}
+      >
+        {votedAgainst ? <VotedText /> : 'Vote Against'}
+      </Button>
+    </>
   );
 };
