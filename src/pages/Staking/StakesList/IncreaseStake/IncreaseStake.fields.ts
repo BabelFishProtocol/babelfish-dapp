4d8a7040a6a28155dbf03e579c5e7e3b8c3153a4
Michal Topa <michal.t1506@gmail.com>
2022-02-28 17:23:03 +0100

    bridge increase stake

--- src/pages/Staking/StakesList/IncreaseStake/IncreaseStake.fields.ts
+++ src/pages/Staking/StakesList/IncreaseStake/IncreaseStake.fields.ts
@@ -1,3 +1,7 @@
 export enum IncreaseStakeFields {
-  IncreaseStakeAmount = 'IncreaseStakeAmount',
+  IncreaseStakeAmount = 'increaseStakeAmount',
 }
+
+export const increaseStakeDefaultValues = {
+  [IncreaseStakeFields.IncreaseStakeAmount]: '',
+};
