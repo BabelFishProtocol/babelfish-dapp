58646c2cc621ba86699b00ce3ae6e6263dc63dcc
Adil <62641096+iamadilmughal@users.noreply.github.com>
2022-02-28 14:50:30 +0500

    Adding Success and Pending Minting Dialogs (#21)
    * Adding Success and Pending Minting Dialogs
    
    * fixing prettier issue
    
    * Code review suggestions
    
    * code review suggestions
    
    changed the openDialogType state to accept only 'succes', 'error' or 'pending' values instead of all strings
    
    * Update MintingInfo.component.tsx

---
+++ src/components/MintingInfo/MintingInfo.component.tsx
@@ -0,0 +1,38 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import React from 'react';
+import { MintingInfoPros } from './MintingInfo.types';
+
+export const MintingInfo = ({ data }: MintingInfoPros) => (
+  <>
+    {data.map(({ label, value, isProminant }, index) => (
+      <Box
+        key={index}
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(2, 1fr)',
+          gap: 2,
+          my: '5px',
+        }}
+      >
+        <Typography
+          variant="h4"
+          sx={{
+            textAlign: 'right',
+          }}
+        >
+          {label}:
+        </Typography>
+        <Typography
+          sx={({ palette }) => ({
+            textAlign: 'left',
+            color: isProminant ? palette.primary.main : 'white',
+          })}
+          variant="body1"
+        >
+          {value}
+        </Typography>
+      </Box>
+    ))}
+  </>
+);
--- src/theme/components.ts
+++ src/theme/components.ts
@@ -246,7 +246,7 @@ export const getComponents = (palette: Palette): Components<Theme> => ({
       paper: {
         backgroundImage: palette.boxGradient,
         minHeight: '500px',
-        minWidth: '500px',
+        width: '500px',
         borderRadius: '8px',
       },
     },
@@ -271,7 +271,8 @@ export const getComponents = (palette: Palette): Components<Theme> => ({
         alignItems: 'center',
         flexDirection: 'column',
         justifyContent: 'center',
-        padding: '20px 30px',
+        padding: '30px',
+        paddingTop: '30px !important',
       },
     },
   },
