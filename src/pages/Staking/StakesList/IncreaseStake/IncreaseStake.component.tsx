3069fd53b002dc17aef3b3a79d2a56f3129a8656
Michał Topa <49481475+meho99@users.noreply.github.com>
2022-03-02 12:11:11 +0100

    bridge increase stake (#27)
    * add typechain config
    
    * init sagas flow
    
    * update chains config
    
    * fix jest config to accept svgs
    
    * remove redundant dependency
    
    * allow sagas with no yield
    
    * move types to dedicated file
    
    * move refetch on account and chainId change to saga
    
    * fix chain types
    
    * fix prettier check
    
    * add fetch stakes list flow
    
    * add testnet contracts config
    
    * fix displaying staking period and voting power delegation
    
    * add autofocus to new stake form
    
    * fix typescript issues
    
    * wip: bridge add new stake
    
    * wip
    
    * forms validation and error messages
    
    * remove unused vars
    
    * move Controlled fields to .controlled files
    
    * fix imports
    
    * wip
    
    * remove tx fee from DialogForm
    
    * add add new stake bridge
    
    * add update state to BalanceBlock and DataTable
    
    * refactor
    
    * add debounce
    
    * add useContractCall tests
    
    * change error message
    
    * bridge increase stake
    
    * fix total staked value (#28)
    
    Co-authored-by: Michal <michalsidzej@gmail.com>

--- src/pages/Staking/StakesList/IncreaseStake/IncreaseStake.component.tsx
+++ src/pages/Staking/StakesList/IncreaseStake/IncreaseStake.component.tsx
@@ -1,5 +1,5 @@
-import { utils } from 'ethers';
-import { useForm } from 'react-hook-form';
+import { BigNumber } from 'ethers';
+import { useForm, useWatch } from 'react-hook-form';
 
 import Typography from '@mui/material/Typography';
 
@@ -10,35 +10,56 @@ import { CurrencyInput } from '../../../../components/CurrencyInput/CurrencyInpu
 import { ControlledInputWithButtonPillGroup } from '../../../../components/InputPillGroup/InputWithButtonPillGroup.controlled';
 
 import {
+  FeeEstimatorProps,
   IncreaseStakeComponentProps,
   IncreaseStakeFormValues,
+  VotingPowerBlockProps,
 } from './IncreaseStake.types';
-import { IncreaseStakeFields } from './IncreaseStake.fields';
+import {
+  increaseStakeDefaultValues,
+  IncreaseStakeFields,
+} from './IncreaseStake.fields';
+import {
+  useEstimateFee,
+  useNeedApproval,
+  useVotingPower,
+} from '../../Staking.hooks';
 
 export const IncreaseStakeComponent = ({
   open,
   onClose,
-  votingPower,
+  onStake,
+  onApprove,
+  unlockDate,
   currentStakeAmount,
+  estimateStakeFee,
+  estimateApproveFee,
+  fishBalance = '0',
 }: IncreaseStakeComponentProps) => {
-  // TODO: change dateSelector and slectDate to controlled inputs by react-hook-form
-  const { control, setValue } = useForm<IncreaseStakeFormValues>({
-    defaultValues: {
-      [IncreaseStakeFields.IncreaseStakeAmount]: '',
-    },
-  });
+  const { control, setValue, formState, watch, handleSubmit } =
+    useForm<IncreaseStakeFormValues>({
+      mode: 'onChange',
+      defaultValues: increaseStakeDefaultValues,
+    });
+
+  const needsApproval = useNeedApproval(
+    watch,
+    IncreaseStakeFields.IncreaseStakeAmount
+  );
+
   return (
     <DialogForm
       open={open}
-      isValid
+      isValid={formState.isValid}
       onClose={onClose}
       title="Add To Stake"
-      leftButtonText="Stake"
+      leftButtonText={needsApproval ? 'Approve' : 'Stake'}
+      handleSubmit={handleSubmit(needsApproval ? onApprove : onStake)}
     >
       <CurrencyInput
         disabled
         symbol="FISH"
-        value={currentStakeAmount}
+        value={formatWeiAmount(currentStakeAmount)}
         title="Amount Currently Staked"
       />
 
@@ -46,19 +67,61 @@ export const IncreaseStakeComponent = ({
         autoFocus
         symbol="FISH"
         title="Amount To Add"
-        totalAmount={utils.parseUnits('2.234')}
+        totalAmount={BigNumber.from(fishBalance)}
         name={IncreaseStakeFields.IncreaseStakeAmount}
         control={control}
         setValue={setValue}
       />
 
-      <TextInput disabled value={votingPower} title="New Voting Power" />
+      <VotingPower control={control} unlockDate={unlockDate} />
 
-      <FeeEstimator />
+      <FeeEstimator
+        control={control}
+        unlockDate={unlockDate}
+        needsApproval={needsApproval}
+        estimateStakeFee={estimateStakeFee}
+        estimateApproveFee={estimateApproveFee}
+      />
     </DialogForm>
   );
 };
 
-const FeeEstimator = () => (
-  <Typography>Tx Fee: {formatWeiAmount('30000000000000', 7)} RBTC</Typography>
-);
+const VotingPower = ({ control, unlockDate }: VotingPowerBlockProps) => {
+  const watchStakeAmount = useWatch({
+    name: IncreaseStakeFields.IncreaseStakeAmount,
+    control,
+  });
+
+  const votingPower = useVotingPower(watchStakeAmount, unlockDate);
+
+  return (
+    <TextInput
+      disabled
+      value={formatWeiAmount(votingPower)}
+      title="New Voting Power"
+    />
+  );
+};
+
+const FeeEstimator = ({
+  control,
+  unlockDate,
+  needsApproval,
+  estimateStakeFee,
+  estimateApproveFee,
+}: FeeEstimatorProps) => {
+  const watchStakeAmount = useWatch({
+    name: IncreaseStakeFields.IncreaseStakeAmount,
+    control,
+  });
+
+  const estimatedFee = useEstimateFee({
+    amount: watchStakeAmount,
+    timestamp: unlockDate,
+    estimator: needsApproval ? estimateApproveFee : estimateStakeFee,
+  });
+
+  return (
+    <Typography>Tx Fee: {formatWeiAmount(estimatedFee, 7)} RBTC</Typography>
+  );
+};
