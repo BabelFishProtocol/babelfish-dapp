dbf7e8e95eea7a0fd4064d8f9b77bad86d75bd98
Michal <michalsidzej@gmail.com>
2022-03-08 15:39:15 +0100

    fix types

--- src/config/providers.ts
+++ src/config/providers.ts
@@ -2,10 +2,12 @@
 import { InjectedConnector } from '@web3-react/injected-connector';
 import { ChainEnum } from './chains';
 
-const SUPPORTED_CHAINS = Object.values(ChainEnum);
+const SUPPORTED_CHAINS = Object.values(ChainEnum).filter(
+  (item) => typeof item === 'number'
+);
 
 export const injectedProvider = new InjectedConnector({
-  supportedChainIds: SUPPORTED_CHAINS as number[],
+  supportedChainIds: SUPPORTED_CHAINS as ChainEnum[],
 });
 
 // const portis = new Portis('d5bd0255-e892-439b-b43c-92b83dfe2be1', 'mainnet');
