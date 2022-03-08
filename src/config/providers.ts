76960acfb0c6a6ad460e109ca9d7e18f85809078
Michał Topa <michal.t1506@gmail.com>
2022-03-08 14:08:55 +0100

    enable connection to all supported chains

--- src/config/providers.ts
+++ src/config/providers.ts
@@ -1,11 +1,11 @@
 // import Portis from '@portis/web3';
 import { InjectedConnector } from '@web3-react/injected-connector';
+import { ChainEnum } from './chains';
 
-// TODO: replace with ChainEnum when it's ready
-const SUPPORTED_CHAINS = [30, 31];
+const SUPPORTED_CHAINS = Object.values(ChainEnum);
 
 export const injectedProvider = new InjectedConnector({
-  supportedChainIds: SUPPORTED_CHAINS,
+  supportedChainIds: SUPPORTED_CHAINS as number[],
 });
 
 // const portis = new Portis('d5bd0255-e892-439b-b43c-92b83dfe2be1', 'mainnet');
