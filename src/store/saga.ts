d8fce86f80d31033b66bcb874a13ff949eb15e20
Michał Sobieraj-Jakubiec <38423054+michalsidzej@users.noreply.github.com>
2022-03-18 12:57:53 +0100

    Feature/aggregator bridge (#50)
    * fix types
    
    * WIP: Add bridge configuration
    
    * fixed spelling
    
    * create aggregator store
    
    * WIP: fetch bridge fees
    
    * create fetch fee sagas
    
    * add flowState logic
    
    * fetch starting token balance
    
    * polish aggregator UI
    
    * fix merge bugs
    
    * fix lint warnings
    
    * use callback for optimization
    
    * update naming suggestions

--- src/store/saga.ts
+++ src/store/saga.ts
@@ -1,7 +1,8 @@
 import { all, fork } from 'typed-redux-saga';
+import { aggregatorSaga } from './aggregator/aggregator.sagas';
 import { appSaga } from './app/app.sagas';
 import { stakingSaga } from './staking/staking.sagas';
 
 export function* indexSaga() {
-  yield* all([fork(appSaga), fork(stakingSaga)]);
+  yield* all([fork(appSaga), fork(stakingSaga), fork(aggregatorSaga)]);
 }
