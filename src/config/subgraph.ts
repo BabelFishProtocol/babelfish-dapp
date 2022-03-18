21e5bf8c7910ea748d8f16d996a8a363aa49a495
Michał Topa <michal.t1506@gmail.com>
2022-03-18 09:45:58 +0100

    remove comments

--- src/config/subgraph.ts
+++ src/config/subgraph.ts
@@ -2,11 +2,6 @@ import { GraphQLClient } from 'graphql-request';
 import { ChainEnum } from './chains';
 
 export const subgraphClients: Partial<Record<ChainEnum, GraphQLClient>> = {
-  // [ChainEnum.RSK_TESTNET]: {
-  //   chainId: ChainEnum.RSK_TESTNET,
-  //   http: 'http://127.0.0.1:8000/subgraphs/name/babelfish/graph',
-  //   ws: 'http://127.0.0.1:8001/subgraphs/name/babelfish/graph',
-  // },
   [ChainEnum.RSK_TESTNET]: new GraphQLClient(
     'http://127.0.0.1:8000/subgraphs/name/babelfish/graph'
   ),
