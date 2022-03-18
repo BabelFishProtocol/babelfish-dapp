f7b2d6b8ae4469e63980275263f5f5679b306b3b
Michał Topa <michal.t1506@gmail.com>
2022-03-18 14:18:17 +0100

    update subgraph names

--- src/config/subgraph.ts
+++ src/config/subgraph.ts
@@ -3,9 +3,9 @@ import { ChainEnum } from './chains';
 
 export const subgraphClients: Partial<Record<ChainEnum, GraphQLClient>> = {
   [ChainEnum.RSK_TESTNET]: new GraphQLClient(
-    'http://3.72.58.184:8000/subgraphs/name/babelfish/graph'
+    'http://3.72.58.184:8000/subgraphs/name/babelfish/rskTestnet-graph'
   ),
   [ChainEnum.RSK]: new GraphQLClient(
-    'http://3.72.58.184:7000/subgraphs/name/babelfish/graph'
+    'http://3.72.58.184:7000/subgraphs/name/babelfish/rsk-graph'
   ),
 };
