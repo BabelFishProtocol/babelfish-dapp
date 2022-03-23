3acabb1ae22f14073a36b16846ff7944734155c5
Michal Winiarski <michal.winiarski@rumblefish.pl>
2022-03-23 11:06:51 +0100

    revert jest config

--- jest.config.ts
+++ jest.config.ts
@@ -11,17 +11,6 @@ export default () => {
   return {
     clearMocks: true,
     preset: 'ts-jest',
-    globals: {
-      'ts-jest': {
-        tsconfig: 'tsconfig.jest.json',
-        diagnostics: false,
-      },
-    },
-    moduleNameMapper: {
-      '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
-      '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-        '<rootDir>/__mocks__/file-mock.ts',
-    },
     testEnvironment: 'jsdom',
     collectCoverage: true,
     coverageDirectory: 'coverage',
