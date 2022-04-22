460d08ce42b9d409953b43304f22ffe32436f45d
Michal Topa <michal.t1506@gmail.com>
2022-04-22 17:27:36 +0200

    move addNewStake saga to separate file

--- src/store/staking/staking.sagas.ts
+++ src/store/staking/staking.sagas.ts
@@ -1,23 +1,15 @@
-import { all, put, call, takeLatest, select } from 'typed-redux-saga';
-import { BigNumber, constants, utils } from 'ethers';
+import { all, put, call, takeLatest } from 'typed-redux-saga';
 
-import { ONE_DAY } from '../../constants';
-import { SagaContractCallStep } from '../types';
-import { createWatcherSaga, contractStepCallsSaga } from '../utils/utils.sagas';
+import { createWatcherSaga } from '../utils/utils.sagas';
 
-import { StakingActions, stakingActions } from './staking.slice';
+import { stakingActions } from './staking.slice';
 import { vestingActions } from '../vesting/vesting.slice';
+import { addNewStake } from './sagas/addNewStake';
 import { fetchHistoryStaking } from './sagas/fetchHistoryStaking';
 import { fetchStakesList } from './sagas/fetchStakesList';
 import { fetchVotingPower } from './sagas/fetchVotingPower';
 import { fetchStakeConstants } from './sagas/fetchStakeConstants';
 import { fetchFishTokenData } from './sagas/fetchFishTokenData';
-import {
-  fishTokenSelector,
-  stakingContractSelector,
-} from '../app/app.selectors';
-import { fishTokenDataSelector } from './staking.selectors';
-import { AddNewStakeCalls } from './staking.state';
 
 /** Fetch data needed for the stake page */
 function* fetchBalances() {
@@ -63,47 +55,4 @@ export function* stakingSaga() {
       fetchHistoryStaking
     ),
   ]);
-}
-
-export function* addNewStake({ payload }: StakingActions['addNewStake']) {
-  const { unlockDate, stakeAmount } = payload;
-  const parsedStakeAmount = utils.parseEther(stakeAmount);
-
-  const staking = yield* select(stakingContractSelector);
-  const fishToken = yield* select(fishTokenSelector);
-  const { allowanceForStaking } = yield* select(fishTokenDataSelector);
-
-  if (!staking || !fishToken) {
-    yield* put(stakingActions.setAddStakeError('Wallet not connected'));
-    return;
-  }
-
-  const steps: SagaContractCallStep<AddNewStakeCalls>[] = [];
-
-  if (BigNumber.from(allowanceForStaking).lt(parsedStakeAmount)) {
-    steps.push({
-      name: 'approve',
-      effect: call(fishToken.approve, staking.address, parsedStakeAmount),
-    });
-  }
-
-  steps.push({
-    name: 'stake',
-    effect: call(
-      staking.stake,
-      parsedStakeAmount,
-      unlockDate + ONE_DAY, // adding 24 hours to date to make sure contract will not choose previous period,
-      constants.AddressZero,
-      constants.AddressZero
-    ),
-  });
-
-  yield* put(stakingActions.setAddStakeSteps(steps));
-
-  yield* contractStepCallsSaga<AddNewStakeCalls>({
-    steps,
-    setErrorAction: stakingActions.setAddStakeError,
-    setStatusAction: stakingActions.setAddStakeStatus,
-    setStepDataAction: stakingActions.setAddStakeStateStepData,
-  });
 }
