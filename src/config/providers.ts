22b57f6bf62864d4836aaf3c6c0dc0d46030e189
Marcin Papież <marcin.papiez@rumblefish.pl>
2022-05-17 14:45:01 +0200

    remove comment

--- src/config/providers.ts
+++ src/config/providers.ts
@@ -1,6 +1,5 @@
 import { InjectedConnector } from '@web3-react/injected-connector';
 // import { PortisConnector } from '@web3-react/portis-connector';
-// import { SUPPORTED_CHAINS } from './chains';
 
 export const injectedConnector = new InjectedConnector({});
 
