bc717f904b9a9b4cc417948404d4e233084b5b5c
Marcin Papież <marcin.papiez@rumblefish.pl>
2022-05-17 14:38:52 +0200

    Changed maxWidth value

--- src/pages/Claim/Claim.component.tsx
+++ src/pages/Claim/Claim.component.tsx
@@ -25,7 +25,7 @@ export const ClaimComponent = ({ onSubmit }: ClaimComponentProps) => {
       <Box
         sx={{
           p: 0,
-          maxWidth: { xs: 1400 },
+          maxWidth: 1400,
           width: '90%',
           margin: '0 auto',
         }}
