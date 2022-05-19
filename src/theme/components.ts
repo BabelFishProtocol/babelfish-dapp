4610b4d1251cf49919123fcf6b3aa6da331e95bc
mwiniarsk <100695408+mwiniarsk@users.noreply.github.com>
2022-05-19 10:45:41 +0200

    change exchange icon, update dropdowns icon (#129)

--- src/theme/components.ts
+++ src/theme/components.ts
@@ -158,6 +158,10 @@ export const getComponents = (palette: Palette): Components<Theme> => ({
         '&.Mui-error': {
           borderWidth: 1,
         },
+        '& .MuiSelect-icon': {
+          color: palette.primary.main,
+          fontSize: '2rem',
+        },
       },
     },
   },
