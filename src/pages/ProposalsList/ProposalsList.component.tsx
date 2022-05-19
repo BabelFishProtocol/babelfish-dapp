2ca55c967265d72ed0abc72185e1ae274b2b6858
meho <michal.t1506@gmail.com>
2022-05-19 12:57:44 +0200

    fix linter

--- src/pages/ProposalsList/ProposalsList.component.tsx
+++ src/pages/ProposalsList/ProposalsList.component.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
