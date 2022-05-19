6c1c795da12c492ae90a1eedbc5640f1e5611bfd
Michał Topa <49481475+meho99@users.noreply.github.com>
2022-05-19 14:33:00 +0200

    update subgraph url (#133)
    * update subgraph url
    
    * fix linter

--- src/config/subgraph.ts
+++ src/config/subgraph.ts
@@ -3,9 +3,9 @@ import { ChainEnum } from './chains';
 
 export const subgraphClients: Partial<Record<ChainEnum, GraphQLClient>> = {
   [ChainEnum.RSK_TESTNET]: new GraphQLClient(
-    'http://3.72.58.184:8000/subgraphs/name/babelfish/rskTestnet-graph'
+    'http://18.224.138.134:8000/subgraphs/name/babelfish/rskTestnet-graph'
   ),
   [ChainEnum.RSK]: new GraphQLClient(
-    'http://3.72.58.184:7000/subgraphs/name/babelfish/rsk-graph'
+    'http://18.224.138.134:7000/subgraphs/name/babelfish/rsk-graph'
   ),
 };
