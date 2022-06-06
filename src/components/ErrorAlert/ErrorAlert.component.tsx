cb5d785fa5f3439e214168f3fab36cd9e55c9cfd
Marcin Papież <marcin.papiez@rumblefish.pl>
2022-06-06 14:32:37 +0200

    Animation, auto close after 5sec

--- src/components/ErrorAlert/ErrorAlert.component.tsx
+++ src/components/ErrorAlert/ErrorAlert.component.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useEffect, useRef } from 'react';
 import { Alert, AlertTitle } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -7,10 +8,22 @@ import { appActions } from '../../store/app/app.slice';
 export const ErrorAlert = () => {
   const dispatch = useDispatch();
   const message = useSelector(errorMessageSelector);
+  const handlerRef = useRef<NodeJS.Timeout>();
 
-  const handleClose = () => {
+  const handleClose = useCallback(() => {
     dispatch(appActions.setErrorMessage(undefined));
-  };
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (handlerRef.current) {
+      clearTimeout(handlerRef.current);
+      handlerRef.current = undefined;
+    }
+
+    if (message) {
+      handlerRef.current = setTimeout(handleClose, 5000);
+    }
+  }, [handleClose, message]);
 
   return message ? (
     <Alert
@@ -19,6 +32,20 @@ export const ErrorAlert = () => {
         position: 'absolute',
         bottom: '32px',
         width: 'calc(100% - 64px)',
+        '@keyframes flip-horizontal': {
+          '0%': {
+            webkitTransform: 'rotateX(-360deg)',
+            transform: 'rotateX(-360deg)',
+          },
+          '100%': {
+            webkitTransform: 'rotateX(0)',
+            transform: 'rotateX(0)',
+          },
+        },
+        webkitAnimation:
+          'flip-horizontal 0.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) both',
+        animation:
+          'flip-horizontal 0.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) both',
       }}
       severity="error"
       variant="filled"
