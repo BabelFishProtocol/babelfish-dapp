6dea076ef56784a20e09ce380168a2b7abbd427f
Derek Matter <dharkmattr@gmail.com>
2022-08-15 13:50:45 +0300

    fix subgraph urls

--- src/config/subgraph.ts
+++ src/config/subgraph.ts
@@ -4,23 +4,24 @@ import { ChainEnum } from './chains';
 
 export const subgraphClients: Partial<Record<ChainEnum, GraphQLClient>> = {
   [ChainEnum.RSK_TESTNET]: new GraphQLClient(
-    'http://18.224.138.134:8000/subgraphs/name/babelfish/rskTestnet-graph'
+    'https://babelfish-subgraph.test.sovryn.app/subgraphs/name/babelfish/rskTestnet-graph'
   ),
   [ChainEnum.RSK]: new GraphQLClient(
-    'http://18.224.138.134:7000/subgraphs/name/babelfish/rsk-graph'
+    'https://babelfish-subgraph.sovryn.app/subgraphs/name/babelfish/rsk-graph'
   ),
 };
 
 export const subgraphWsClients: Partial<Record<ChainEnum, SubscriptionClient>> =
   {
     [ChainEnum.RSK_TESTNET]: new SubscriptionClient(
-      'ws://18.224.138.134:8001/subgraphs/name/babelfish/rskTestnet-graph',
+      'wss://babelfish-subgraph-ws.test.sovryn.app/subgraphs/name/babelfish/rskTestnet-graph',
+      
       {
         reconnect: true,
       }
     ),
     [ChainEnum.RSK]: new SubscriptionClient(
-      'ws://18.224.138.134:7001/subgraphs/name/babelfish/rsk-graph',
+      'wss://babelfish-subgraph-ws.sovryn.app/subgraphs/name/babelfish/rsk-graph',
       {
         reconnect: true,
       }
