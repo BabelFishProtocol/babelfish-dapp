4aea5e3a0dfa981fbcd6d64525590e7a0f00e2f9
tiltom <tiltom.defi@protonmail.com>
2023-03-28 13:01:17 +0200

    SOV-1687: Add aggregator balance to XUSD -> bAsset transfers (#180)
    * Do not show bridge details if not applicable
    
    * Show aggregator balance

--- src/pages/Aggregator/AggregatorInfo/AggregatorInfo.component.tsx
+++ src/pages/Aggregator/AggregatorInfo/AggregatorInfo.component.tsx
@@ -1,16 +1,20 @@
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import exchangeIcon from '../../../assets/icons/exchange.svg';
+import { DEFAULT_ASSET_DECIMALS } from '../../../constants';
 import { formatUnitAmount } from '../../../utils/helpers';
 import { AggregatorInfoComponentProps } from './AggregatorInfo.types';
 import { InfoRow } from './InfoRow.component';
 
 export const AggregatorInfoComponent = ({
   onClick,
-  state,
+  feesAndLimitsState,
   feesAndLimits,
-  tokenName,
+  startingToken,
+  destinationToken,
   tokenDecimals,
+  aggregatorBalance,
+  aggregatorBalanceState,
 }: AggregatorInfoComponentProps) => (
   <Box
     sx={{
@@ -32,6 +36,7 @@ export const AggregatorInfoComponent = ({
     >
       <img alt="exchange icon" src={exchangeIcon} />
     </IconButton>
+
     <Box
       sx={{
         display: 'grid',
@@ -42,38 +47,58 @@ export const AggregatorInfoComponent = ({
         color: ({ palette }) => palette.grey[600],
       }}
     >
-      <InfoRow
-        label="Min Transfer"
-        value={`${formatUnitAmount(
-          feesAndLimits.minTransfer || 0,
-          tokenDecimals || 18,
-          2
-        )} ${tokenName || ''}`}
-        state={state}
-      />
-      <InfoRow
-        label="Max Transfer"
-        value={`${formatUnitAmount(feesAndLimits.maxTransfer || 0, 18, 2)} ${
-          tokenName || ''
-        }`}
-        state={state}
-      />
-      <InfoRow
-        label="Bridge Fee"
-        value={`${formatUnitAmount(
-          feesAndLimits.bridgeFee || 0,
-          tokenDecimals || 18,
-          2
-        )} ${tokenName || ''}`}
-        state={state}
-      />
-      <InfoRow
-        label="Day Limit"
-        value={`${formatUnitAmount(feesAndLimits.dailyLimit || 0, 18, 2)} ${
-          tokenName || ''
-        }`}
-        state={state}
-      />
+      {aggregatorBalance && (
+        <InfoRow
+          label="Aggregator Balance"
+          value={`${formatUnitAmount(
+            aggregatorBalance,
+            DEFAULT_ASSET_DECIMALS,
+            2
+          )} ${destinationToken || ''}`}
+          state={aggregatorBalanceState}
+        />
+      )}
+
+      {feesAndLimitsState === 'success' && (
+        <>
+          <InfoRow
+            label="Min Transfer"
+            value={`${formatUnitAmount(
+              feesAndLimits.minTransfer || 0,
+              tokenDecimals || DEFAULT_ASSET_DECIMALS,
+              2
+            )} ${startingToken || ''}`}
+            state={feesAndLimitsState}
+          />
+          <InfoRow
+            label="Max Transfer"
+            value={`${formatUnitAmount(
+              feesAndLimits.maxTransfer || 0,
+              DEFAULT_ASSET_DECIMALS,
+              2
+            )} ${startingToken || ''}`}
+            state={feesAndLimitsState}
+          />
+          <InfoRow
+            label="Bridge Fee"
+            value={`${formatUnitAmount(
+              feesAndLimits.bridgeFee || 0,
+              tokenDecimals || DEFAULT_ASSET_DECIMALS,
+              2
+            )} ${startingToken || ''}`}
+            state={feesAndLimitsState}
+          />
+          <InfoRow
+            label="Day Limit"
+            value={`${formatUnitAmount(
+              feesAndLimits.dailyLimit || 0,
+              DEFAULT_ASSET_DECIMALS,
+              2
+            )} ${startingToken || ''}`}
+            state={feesAndLimitsState}
+          />
+        </>
+      )}
     </Box>
   </Box>
 );
