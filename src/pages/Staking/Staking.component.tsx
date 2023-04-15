9a2ac50fe9b46e40d3dd5392b3a2059927f6c2ea
tiltom <tilnak.tomas@gmail.com>
2023-04-15 09:17:32 +0200

    Remove maintenance mode overlay

--- src/pages/Staking/Staking.component.tsx
+++ src/pages/Staking/Staking.component.tsx
@@ -22,7 +22,6 @@ import { RewardBlockProps, StakingComponentProps } from './Staking.types';
 import wave1Icon from '../../assets/icons/wave1.svg';
 import wave2Icon from '../../assets/icons/wave2.svg';
 import wave3Icon from '../../assets/icons/wave3.svg';
-import maintenanceModeIcon from '../../assets/icons/maintenance-mode.svg';
 
 export const StakingComponent = ({
   rewards,
@@ -37,7 +36,6 @@ export const StakingComponent = ({
 
   return (
     <Box sx={{ position: 'relative' }}>
-      <MaintenanceModeOverlay />
       <Breadcrumbs links={[{ title: UrlNames.Staking }]} />
       <PageAligner>
         <Box
@@ -124,28 +122,5 @@ const RewardBlock = ({ amount, asset, usdAmount }: RewardBlockProps) => (
     <Button size="small" variant="text" sx={{ ml: 1 }}>
       <Typography variant="body2">withdraw</Typography>
     </Button>
-  </Box>
-);
-
-const MaintenanceModeOverlay = () => (
-  <Box
-    sx={{
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-      zIndex: 1000,
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: 'rgba(0, 0, 0, 0.8)',
-    }}
-  >
-    <img
-      src={maintenanceModeIcon}
-      alt="Staking is currently under maintenance"
-      width={600}
-    />
   </Box>
 );
