38d4166ea2ed79a5c5c1779219102b959f0ac2db
tiltom <tilnak.tomas@gmail.com>
2023-04-19 11:28:24 +0200

    Fix method for generating staking dates

--- src/components/DateSelector/DateSelector.utils.ts
+++ src/components/DateSelector/DateSelector.utils.ts
@@ -1,17 +1,60 @@
+import dayjs from 'dayjs';
 import { getCurrentTimestamp, timestampToDate } from '../../utils/helpers';
-import { MAX_STAKING_PERIODS, TWO_WEEKS } from '../../constants';
+import { MAX_STAKING_PERIODS, MS, TWO_WEEKS } from '../../constants';
 import {
   GroupedDates,
   CheckpointInfo,
   GetAvailableDatesConfig,
 } from './DateSelector.types';
 
-export const getAllCheckpoints = (kickoffTs: number) => {
+const getAllCheckpoints = (
+  kickoffTs: number,
+  stakeEndDate: number | undefined
+) => {
+  const currentDate = new Date();
+
+  const contractDate = dayjs(kickoffTs * MS).toDate();
+  const contractOffset = contractDate.getTimezoneOffset() / 60;
+  const currentUserOffset = currentDate.getTimezoneOffset() / 60;
+
+  let contractDateDeployed = dayjs(kickoffTs * MS).add(contractOffset, 'hour'); // get contract date in UTC-0
+  const userDateUTC = dayjs(currentDate).add(currentUserOffset, 'hour'); // get user offset
+
   const dates: number[] = [];
+  const stakeExtensionDates: number[] = [];
+
+  // getting the last possible date before the current date
+  for (let i = 1; contractDateDeployed.unix() < userDateUTC.unix(); i++) {
+    const intervalDate = contractDateDeployed.add(2, 'week');
+    contractDateDeployed = intervalDate;
+  }
 
   for (let i = 1; i < MAX_STAKING_PERIODS; i++) {
-    const date = kickoffTs + TWO_WEEKS * i;
-    dates.push(date);
+    if (contractDateDeployed.unix() > userDateUTC.unix()) {
+      const date = contractDateDeployed.add(2, 'week');
+      contractDateDeployed = date;
+
+      // If we just add a new stake, we can return all dates in the future
+      if (!stakeEndDate) {
+        dates.push(date.unix());
+      }
+
+      // In case we extend an existing stake, we cannot return dates before the stake end date
+      if (
+        stakeEndDate &&
+        dayjs(stakeEndDate * MS)
+          .add(contractOffset, 'hour')
+          .toDate()
+          .getTime() /
+          MS <
+          date.unix()
+      ) {
+        stakeExtensionDates.push(date.unix());
+      }
+    }
+  }
+  if (stakeExtensionDates.length) {
+    return stakeExtensionDates;
   }
 
   return dates;
@@ -23,7 +66,7 @@ export const getAvailableDates = ({
   prevDate,
 }: GetAvailableDatesConfig) => {
   const currentCheckpoint = Math.trunc(getCurrentTimestamp()) + TWO_WEEKS;
-  const allDates = getAllCheckpoints(kickoffTs);
+  const allDates = getAllCheckpoints(kickoffTs, prevDate);
 
   const dates: CheckpointInfo[] = allDates.map((timestamp) => {
     const convertedDate = timestampToDate(timestamp);
