666215c139646f86c5bd196d52b10f495508dbb3
tiltom <tilnak.tomas@gmail.com>
2023-05-12 10:58:34 +0200

    Add mainnet chains configuration

--- src/config/sovryn-onboard-chains.ts
+++ src/config/sovryn-onboard-chains.ts
@@ -8,30 +8,46 @@ export enum Chains {
   BSC = 'bsc',
 }
 
+const mainnetChains: Chain[] = [
+  {
+    id: ChainIds.RSK_MAINNET,
+    label: 'Rootstock',
+    token: 'RBTC',
+    rpcUrl: RSK_RPC[Environments.Mainnet],
+    blockExplorerUrl: RSK_EXPLORER[Environments.Mainnet],
+  },
+  {
+    id: ChainIds.BSC_MAINNET,
+    label: 'BSC',
+    token: 'BNB',
+    rpcUrl: 'https://bsc-dataseed.binance.org/',
+    blockExplorerUrl: 'https://bscscan.com/',
+  },
+  {
+    id: '0x1', // TODO: Temporary fix for ChainIds.Mainnet as it has the value of 0x01 instead of 0x1, verified the correct hex value at https://chainlist.org/
+    label: 'Ethereum Mainnet',
+    token: 'ETH',
+    rpcUrl: 'https://eth.llamarpc.com',
+    blockExplorerUrl: 'https://etherscan.io',
+  },
+];
+
+const testnetChains: Chain[] = [
+  {
+    id: ChainIds.RSK_TESTNET,
+    label: 'Rootstock testnet',
+    token: 'tRBTC',
+    rpcUrl: RSK_RPC[Environments.Testnet],
+    blockExplorerUrl: RSK_EXPLORER[Environments.Testnet],
+  },
+];
+
 const isMainnet = true; // We don't distinguish between mainnet and testnet so this should be always set to true, set it to false only for testnet debugging if necessary
 
 export const defaultChainId = (
   isMainnet ? ChainIds.RSK_MAINNET : ChainIds.RSK_TESTNET
 ) as string;
 
-// @dev: temp solution for hardware wallets to connect to the correct chain
-// good enough for now, but should be refactored when cross-chain support is needed
-export const chains: Chain[] = [
-  isMainnet
-    ? {
-        id: ChainIds.RSK_MAINNET,
-        label: 'Rootstock',
-        token: 'RBTC',
-        rpcUrl: RSK_RPC[Environments.Mainnet],
-        blockExplorerUrl: RSK_EXPLORER[Environments.Mainnet],
-      }
-    : {
-        id: ChainIds.RSK_TESTNET,
-        label: 'Rootstock testnet',
-        token: 'tRBTC',
-        rpcUrl: RSK_RPC[Environments.Testnet],
-        blockExplorerUrl: RSK_EXPLORER[Environments.Testnet],
-      },
-];
+export const chains: Chain[] = isMainnet ? mainnetChains : testnetChains;
 
 setup(chains);
